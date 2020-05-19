53c5a1c4e756122c2ecdbd919dae592099c1b1ac
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-19 14:15:16 +0200

    imported mongoose and MONGODB key

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { ApolloServer } = require('apollo-server')
 const gql = require('graphql-tag')
+const mongoose = require('mongoose')
+
+const { MONGODB } = require('config.js')
 
 const typeDefs = gql`
   type Query {
