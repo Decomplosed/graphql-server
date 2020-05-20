dfb5df2f9629a63168de400565b92814de0458a3
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-20 14:49:40 +0200

    imported resolvers

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const gql = require('graphql-tag')
 const mongoose = require('mongoose')
 
 const typeDefs = require('./graphql/typeDefs')
-const Post = require('./models/Post')
+const resolvers = require('./graphql/resolvers')
 const { MONGODB } = require('./config.js')
-
 
 const server = new ApolloServer({
   typeDefs,
