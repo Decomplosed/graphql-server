887d215526239549f6779773a3e758bcc5bb049c
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-20 17:04:29 +0200

    added missing async

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,12 +1,12 @@
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
-const {SECRET_KEY} = require('../../config')
+const { SECRET_KEY } = require('../../config')
 const User = require('../../models/User')
 
 module.export = {
   Mutation: {
-    register(
+    async register(
       _,
       { registerInput: { username, email, password, confirmPassword } },
       context,
@@ -22,21 +22,25 @@ module.export = {
         email,
         username,
         password,
-        createdAt: new Date().toISOString()
+        createdAt: new Date().toISOString(),
       })
 
       const res = await newUser.save()
 
-      const token = jwt.sign({
-        id: res.id,
-        email: res.email,
-        username: res.username
-      }, SECRET_KEY, {expiresIn: '1h'})
+      const token = jwt.sign(
+        {
+          id: res.id,
+          email: res.email,
+          username: res.username,
+        },
+        SECRET_KEY,
+        { expiresIn: '1h' }
+      )
 
       return {
         ...res._doc,
         id: res._id,
-        token
+        token,
       }
     },
   },
