03e26589416cc77b63520caab3a556b1d4dfdeac
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-20 16:53:12 +0200

    extracted username, email, password, confirmPassword from args

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -2,7 +2,12 @@ const User = require('../../models/User')
 
 module.export = {
   Mutation: {
-    register(_, args, context, info) {
+    register(
+      _,
+      { registerInput: { username, email, password, confirmPassword } },
+      context,
+      info
+    ) {
       // TODO: validate user data
       // TODO: Make sure user doesnt already exist
       // TODO: hash password
