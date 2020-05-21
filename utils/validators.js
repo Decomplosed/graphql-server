7f84521cde9bafdbde1f8b605e6e8d9c7ce983d8
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-21 14:24:15 +0200

    defined empty errors object

--- utils/validators.js
+++ utils/validators.js
@@ -32,4 +32,6 @@ module.exports.validateRegisterInput = (
   }
 }
 
-module.exports.validateLoginInput = (username, password) => {}
+module.exports.validateLoginInput = (username, password) => {
+  const errors = {}
+}
