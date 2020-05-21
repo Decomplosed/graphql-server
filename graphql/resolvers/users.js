8f98d2f971271b918cc0e9fb27fcc37735d94c55
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-21 14:20:58 +0200

    moved utils to the root directory and fiex import

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const { UserInputError } = require('apollo-server')
 
-const validateRegisterInput = require('../utils/validators')
+const validateRegisterInput = require('../../utils/validators')
 const { SECRET_KEY } = require('../../config')
 const User = require('../../models/User')
 
