f39ba213a15f49c87b825505ede7104d2d5f0656
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-21 14:15:05 +0200

    added validation for empty email

--- graphql/utils/validators.js
+++ graphql/utils/validators.js
@@ -9,4 +9,8 @@ module.exports.validateRegisterInput = (
   if (username.trim() === '') {
     errors.username = 'Username must not be empty'
   }
+
+  if (email.trim() === '') {
+    errors.email = 'Email must not be empty'
+  }
 }
