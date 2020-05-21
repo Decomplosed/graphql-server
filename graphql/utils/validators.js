fc2b1cd0ff513ddcedf897bfa2554cd954ea5a50
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-21 14:16:32 +0200

    created a regEx for emails

--- graphql/utils/validators.js
+++ graphql/utils/validators.js
@@ -12,5 +12,7 @@ module.exports.validateRegisterInput = (
 
   if (email.trim() === '') {
     errors.email = 'Email must not be empty'
+  } else {
+    const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/
   }
 }
