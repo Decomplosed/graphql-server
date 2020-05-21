82052fd2ee8ee4aa1f748ed62fae45583feeabba
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-21 15:19:31 +0200

    defined deletePost Mutation

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -28,5 +28,6 @@ module.exports = gql`
     register(registerInput: RegisterInput): User!
     login(username: String!, password: String!): User!
     createPost(body: String!): Post!
+    deletePost(postId: ID!): String!
   }
 `
