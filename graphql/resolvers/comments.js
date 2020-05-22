547deb13b3ad54cdfa16cb784073ad2d0d1f1151
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-22 16:58:37 +0200

    started writing createComment Mutation

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -1,1 +1,18 @@
 const Post = require('../../models/Post')
+const UserInputError = require('apollo-server')
+
+module.exports = {
+  Mutation: {
+    createComment: async (_, { postId, body }, context) => {
+      const user = checkAuth(context)
+
+      if (body.trim() === '') {
+        throw new UserInputError('Empty comment', {
+          errors: {
+            body: 'Comment body must not be empty',
+          },
+        })
+      }
+    },
+  },
+}
