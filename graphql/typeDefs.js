a26e4e767163bdd07393419a870e30137d3abc0d
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-22 16:52:46 +0200

    created Mutation of createComment

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -42,5 +42,6 @@ module.exports = gql`
     login(username: String!, password: String!): User!
     createPost(body: String!): Post!
     deletePost(postId: ID!): String!
+    createComment(postId: String!, body: String!): Post!
   }
 `
