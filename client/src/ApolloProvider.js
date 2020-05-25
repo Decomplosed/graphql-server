16d8d8bbdba7ef0699f9cc53392bc738b25dce82
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-25 23:37:03 +0200

    created http link

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -4,3 +4,7 @@ import ApolloClient from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { createHttpLink } from 'apollo-link-http'
 import { ApolloProvider } from '@apollo/react-hooks'
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:5000',
+})
