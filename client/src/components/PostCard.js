aa8ad34177e5cc88b8bd31c0255915bfbc8aea2d
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-26 14:01:34 +0200

    started creating body of the component - extaaaract data from props

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,2 +1,8 @@
 import React from 'react'
 import { Card, Icon, Label } from 'semantic-ui-react'
+
+function PostCard({props: {id, body, createdAt, username, likeCount, commentCount, likes}}) {
+  return ()
+}
+
+export default PostCard
