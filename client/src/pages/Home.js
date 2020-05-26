49517ff47a65061371d66db0916dee6bde91fb39
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-26 13:50:40 +0200

    created query to fetch posts

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -10,4 +10,26 @@ function Home() {
   )
 }
 
+const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+    }
+  }
+`
+
 export default Home
