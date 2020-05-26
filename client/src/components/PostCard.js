a411b09359235e741f846d3fd0d9a751a70a6c3d
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-26 14:03:38 +0200

    started adjusting the template of the PostCard component

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,8 +1,35 @@
 import React from 'react'
 import { Card, Icon, Label } from 'semantic-ui-react'
 
-function PostCard({props: {id, body, createdAt, username, likeCount, commentCount, likes}}) {
-  return ()
+function PostCard({
+  props: { id, body, createdAt, username, likeCount, commentCount, likes },
+}) {
+  return (
+    <Card>
+      <Card.Content>
+        <Image
+          floated='right'
+          size='mini'
+          src='https://react.semantic-ui.com/images/avatar/large/elliot.jpg'
+        />
+        <Card.Header>{username}</Card.Header>
+        <Card.Meta>{createdAt}</Card.Meta>
+        <Card.Description>
+          Molly wants to add you to the group <strong>musicians</strong>
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <div className='ui two buttons'>
+          <Button basic color='green'>
+            Approve
+          </Button>
+          <Button basic color='red'>
+            Decline
+          </Button>
+        </div>
+      </Card.Content>
+    </Card>
+  )
 }
 
 export default PostCard
