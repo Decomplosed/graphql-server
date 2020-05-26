7e75d81a91b1439380ccede7c0113fdbeb009af9
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-26 13:28:59 +0200

    removed Segment and wrapping div

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -8,36 +8,30 @@ function MenuBar() {
 
   const { activeItem } = this.state
   return (
-    <div>
-      <Menu pointing secondary>
+    <Menu pointing secondary>
+      <Menu.Item
+        name='home'
+        active={activeItem === 'home'}
+        onClick={this.handleItemClick}
+      />
+      <Menu.Item
+        name='messages'
+        active={activeItem === 'messages'}
+        onClick={this.handleItemClick}
+      />
+      <Menu.Item
+        name='friends'
+        active={activeItem === 'friends'}
+        onClick={this.handleItemClick}
+      />
+      <Menu.Menu position='right'>
         <Menu.Item
-          name='home'
-          active={activeItem === 'home'}
+          name='logout'
+          active={activeItem === 'logout'}
           onClick={this.handleItemClick}
         />
-        <Menu.Item
-          name='messages'
-          active={activeItem === 'messages'}
-          onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          name='friends'
-          active={activeItem === 'friends'}
-          onClick={this.handleItemClick}
-        />
-        <Menu.Menu position='right'>
-          <Menu.Item
-            name='logout'
-            active={activeItem === 'logout'}
-            onClick={this.handleItemClick}
-          />
-        </Menu.Menu>
-      </Menu>
-
-      <Segment>
-        <img src='/images/wireframe/media-paragraph.png' />
-      </Segment>
-    </div>
+      </Menu.Menu>
+    </Menu>
   )
 }
 
