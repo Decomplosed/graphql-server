982dda13e06961814e199d54598b1f9067ba3595
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-26 13:31:40 +0200

    imported MenuBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
 
 import 'semantic-ui-css/semantic.min.css'
 import './App.css'
+
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import MenuBar from './components/MenuBar'
 
 function App() {
   return (
