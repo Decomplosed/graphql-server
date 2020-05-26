bdfa4a34c25165d0a59dddfe3f6670424cc9dffc
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-26 12:51:18 +0200

    imported all pages inside App

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 import 'semantic-ui-css/semantic.min.css'
 import './App.css'
