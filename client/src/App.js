d8a8ea1083c3656da544909596ab6a4625bdb9d0
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-26 12:46:36 +0200

    brought the styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
+import 'semantic-ui-css/semantic.min.css'
+import './App.css'
+
 function App() {
   return (
     <div className='App'>
