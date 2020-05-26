d5c8d4407c1e0cce05adebf27aad922b140e7f77
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-26 13:28:36 +0200

    removed render method

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -6,42 +6,39 @@ function MenuBar() {
 
   const handleItemClick = (e, { name }) => setActiveItem(name)
 
-  render() {
-    const { activeItem } = this.state
+  const { activeItem } = this.state
+  return (
+    <div>
+      <Menu pointing secondary>
+        <Menu.Item
+          name='home'
+          active={activeItem === 'home'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='messages'
+          active={activeItem === 'messages'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='friends'
+          active={activeItem === 'friends'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Menu position='right'>
+          <Menu.Item
+            name='logout'
+            active={activeItem === 'logout'}
+            onClick={this.handleItemClick}
+          />
+        </Menu.Menu>
+      </Menu>
 
-    return (
-      <div>
-        <Menu pointing secondary>
-          <Menu.Item
-            name='home'
-            active={activeItem === 'home'}
-            onClick={this.handleItemClick}
-          />
-          <Menu.Item
-            name='messages'
-            active={activeItem === 'messages'}
-            onClick={this.handleItemClick}
-          />
-          <Menu.Item
-            name='friends'
-            active={activeItem === 'friends'}
-            onClick={this.handleItemClick}
-          />
-          <Menu.Menu position='right'>
-            <Menu.Item
-              name='logout'
-              active={activeItem === 'logout'}
-              onClick={this.handleItemClick}
-            />
-          </Menu.Menu>
-        </Menu>
-
-        <Segment>
-          <img src='/images/wireframe/media-paragraph.png' />
-        </Segment>
-      </div>
-    )
-  }
+      <Segment>
+        <img src='/images/wireframe/media-paragraph.png' />
+      </Segment>
+    </div>
+  )
 }
 
 export default MenuBar
