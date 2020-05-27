659733f44844851923d444b54e1a4a4964190de1
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-27 17:01:10 +0200

    Meta will be a Link leading to the /posts/postid url

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Card, Icon, Label, Image } from 'semantic-ui-react'
 import moment from 'moment'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function PostCard({
   post: { id, body, createdAt, username, likeCount, commentCount, likes },
@@ -15,7 +15,9 @@ function PostCard({
           src='https://react.semantic-ui.com/images/avatar/large/elliot.jpg'
         />
         <Card.Header>{username}</Card.Header>
-        <Card.Meta>{moment(createdAt).fromNow(true)}</Card.Meta>
+        <Card.Meta as={Link} to={`/posts/${id}`}>
+          {moment(createdAt).fromNow(true)}
+        </Card.Meta>
         <Card.Description>{body}</Card.Description>
       </Card.Content>
       <Card.Content extra>
