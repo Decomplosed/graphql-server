c69a29ba28e2b732c5df2b1649dbace259351f5c
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-27 16:15:37 +0200

    fixes in comments resolver

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -37,7 +37,7 @@ module.exports = {
         const commentIndex = post.comments.findIndex((c) => c.id === commentId)
 
         if (post.comments[commentIndex].username === username) {
-          post.comments.spice(commentIndex, 1)
+          post.comments.splice(commentIndex, 1)
           await post.save()
           return post
         } else {
@@ -46,27 +46,6 @@ module.exports = {
       } else {
         throw new UserInputError('Post not found')
       }
-    },
-    async likePost(_, { postId }, context) {
-      const { username } = checkAuth(context)
-
-      const post = await post.findById(postId)
-
-      if (post) {
-        if (post.likes.find((like) => like.username === username)) {
-          // Post already liked, unlike it
-          post.likes = post.likes.filter((like) => like.username !== username)
-        } else {
-          // Not liked, like post
-          post.likes.push({
-            username,
-            createComment: new Date().toISOString(),
-          })
-        }
-
-        await post.save()
-        return post
-      } else throw new UserInputError('Post not found')
     },
   },
 }
