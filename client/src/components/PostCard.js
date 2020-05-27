f2562ea624bf5db512ddd19c319173c133d423c8
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-27 17:07:53 +0200

    added comment button

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -37,6 +37,14 @@ function PostCard({
             {likeCount}
           </Label>
         </Button>
+        <Button as='div' labelPosition='right' onClick={commentOnPost}>
+          <Button color='blue' basic>
+            <Icon name='comments' />
+          </Button>
+          <Label basic color='blue' pointing='left'>
+            {commentCount}
+          </Label>
+        </Button>
       </Card.Content>
     </Card>
   )
