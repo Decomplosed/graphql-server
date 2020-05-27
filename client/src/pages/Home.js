69605cb1634ffeba6c049c90e34e25f348855c4a
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-27 17:14:40 +0200

    fixed issue with not loading posts

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,28 +6,28 @@ import { Grid } from 'semantic-ui-react'
 import PostCard from '../components/PostCard'
 
 function Home() {
-  const {
-    loading,
-    data: { getPosts: posts },
-  } = useQuery(FETCH_POSTS_QUERY)
+  let posts = ''
+  const { loading, data } = useQuery(FETCH_POSTS_QUERY)
+
+  if (data) {
+    posts = { data: data.getPosts }
+  }
 
   return (
     <Grid columns={3}>
-      <Grid.row className='page-title'>
-        <h1>Recent Posts:</h1>
-      </Grid.row>
-      <Grid.Row>
-        {loading ? (
-          <h1>Loading Posts...</h1>
-        ) : (
-          posts &&
-          posts.map((post) => (
-            <Grid.column key={post.id} style={{ marginBottom: 20 }}>
-              <PostCard post={post} />
-            </Grid.column>
-          ))
-        )}
+      <Grid.Row className='page-title'>
+        <h1>Recent Posts</h1>
       </Grid.Row>
+      {loading ? (
+        <h1>Loading posts..</h1>
+      ) : (
+        posts.data &&
+        posts.data.map((post) => (
+          <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+            <PostCard post={post} />
+          </Grid.Column>
+        ))
+      )}
     </Grid>
   )
 }
