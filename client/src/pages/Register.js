9b799b95fa0b7038ec1a3aafe039cbfd18b6fed5
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-28 22:59:17 +0200

    imported Button from semantic ui

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Form } from 'semantic-ui-react'
+import { Form, Button } from 'semantic-ui-react'
 
 function Register() {
   const [values, setValues] = useState({
