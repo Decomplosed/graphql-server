d9f9675747e2301327acf245645079554cb0e041
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-29 23:45:34 +0200

    imported AuthProvider inside main App component

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import { Container } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 import './App.css'
 
+import { AuthProvider } from './context/auth'
+
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
