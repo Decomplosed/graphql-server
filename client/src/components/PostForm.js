bb8533b1cc50ed888b8ee107c3bc619f9dc91495
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-30 23:52:49 +0200

    created a template of the form

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -2,9 +2,25 @@ import React from 'react'
 import { Form } from 'semantic-ui-react'
 
 import { useForm } from '../utils/hooks'
+import { ValuesOfCorrectTypeRule } from 'graphql'
 
 function PostForm() {
-  return <div></div>
+  return (
+    <Form onSubmit={onSubmit}>
+      <h2>Create a Post:</h2>
+      <Form.Field>
+        <Form.Input
+          placeholder='Hi World!'
+          name='body'
+          onChange={onChange}
+          value={ValuesOfCorrectTypeRule.body}
+        />
+        <Button type='submit' color='teal'>
+          Submit
+        </Button>
+      </Form.Field>
+    </Form>
+  )
 }
 
 export default PostForm
