651a7f6492a6f5b92632b558d179e62e2acf70a7
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-30 23:29:23 +0200

    imported AuthContext

--- client/src/utils/AuthRoute.js
+++ client/src/utils/AuthRoute.js
@@ -1,4 +1,6 @@
 import React, { useContext } from 'react'
 import { Route, Redirect } from 'react-router-dom'
 
+import { AuthContext } from '../context/auth'
+
 function AuthRoute({ component: Component, ...rest }) {}
