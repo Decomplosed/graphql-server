80bb44a5fb46730f0ff4dd2bf60c5a0721e9cd3a
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-31 22:26:12 +0200

    added error attribute to the input

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -39,6 +39,7 @@ function PostForm() {
             name='body'
             onChange={onChange}
             value={values.body}
+            error={error ? true : false}
           />
           <Button type='submit' color='teal'>
             Submit
