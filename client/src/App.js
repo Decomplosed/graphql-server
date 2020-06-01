803045a1bbe9d44687dd1067ae716797bf64134a
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-01 14:36:07 +0200

    imported SinglePost inside App

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import MenuBar from './components/MenuBar'
+import SinglePost from './components/SinglePost'
 
 function App() {
   return (
