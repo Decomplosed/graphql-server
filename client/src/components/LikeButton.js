e7a0baaaddcc6bfbcc2ddddf9ac4bd59439de086
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-01 14:02:55 +0200

    created useState case for liked and setLiked

--- client/src/components/LikeButton.js
+++ client/src/components/LikeButton.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom'
 import { useMutation } from '@apollo/react-hooks'
 import gql from 'graphql-tag'
 
-function LikeButton({ post: { id, likeCount, likes } }) {}
+function LikeButton({ post: { id, likeCount, likes } }) {
+  const [liked, setLiked] = useState(false)
+}
 
 export default LikeButton
