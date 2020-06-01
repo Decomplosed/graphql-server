c555a8493fad75d8f3344f922f7b071b9fa2a659
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-01 14:09:09 +0200

    extracted user from props

--- client/src/components/LikeButton.js
+++ client/src/components/LikeButton.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom'
 import { useMutation } from '@apollo/react-hooks'
 import gql from 'graphql-tag'
 
-function LikeButton({ post: { id, likeCount, likes } }) {
+function LikeButton({ user, post: { id, likeCount, likes } }) {
   const [liked, setLiked] = useState(false)
 
   useEffect(() => {
     if (user && likes.find((like) => like.username === user.username)) {
       setLiked(true)
     } else setLiked(false)
-  }, [likes])
+  }, [likes, user])
 
   const [likePost] = useMutation(LIKE_POST_MUTATION, {
     variables: { postId: id },
