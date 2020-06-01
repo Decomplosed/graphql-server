97941be3c064ba06b47cd1bd3dc2a7d53082da25
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-01 14:36:17 +0200

    created a Route for a single post

--- client/src/App.js
+++ client/src/App.js
@@ -23,6 +23,7 @@ function App() {
           <Route exact path='/' component={Home} />
           <AuthRoute exact path='/login' component={Login} />
           <AuthRoute exact path='/register' component={Register} />
+          <Route exact path='/posts/:id' component={SinglePost} />
         </Container>
       </Router>
     </AuthProvider>
