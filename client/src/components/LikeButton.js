84339461d82c43daab4065efc9116ab8a75a44c1
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-01 13:59:44 +0200

    defined functional component and extracted id, likeCOunt and likes from post prop

--- client/src/components/LikeButton.js
+++ client/src/components/LikeButton.js
@@ -2,3 +2,5 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useMutation } from '@apollo/react-hooks'
 import gql from 'graphql-tag'
+
+function LikeButton({ post: { id, likeCount, likes } }) {}
