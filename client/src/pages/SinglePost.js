ad7f442e5d24782c47551fe67d68a8711bf877ac
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-03 11:22:09 +0200

    imported moment

--- client/src/pages/SinglePost.js
+++ client/src/pages/SinglePost.js
@@ -2,6 +2,7 @@ import React from 'react'
 import gql from 'graphql-tag'
 import { useQuery } from '@apollo/react-hooks'
 import { Grid, Image } from 'semantic-ui-react'
+import moment from 'moment'
 
 function SinglePost(props) {
   const postId = props.match.params.postId
