da41a4e048062c43728b8cd0fc9a82aef374e86e
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-03 11:27:16 +0200

    imported useContext

--- client/src/pages/SinglePost.js
+++ client/src/pages/SinglePost.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import gql from 'graphql-tag'
 import { useQuery } from '@apollo/react-hooks'
 import { Grid, Image, Card } from 'semantic-ui-react'
