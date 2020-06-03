c6f408b8bdb3af08e18e8629ad9ff8b82181f7be
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-03 12:18:28 +0200

    implemented DeleteButton code from PostCard component

--- client/src/components/DeleteButton.js
+++ client/src/components/DeleteButton.js
@@ -2,6 +2,19 @@ import React from 'react'
 import gql from 'graphql-tag'
 import { useMutation } from '@apollo/react-hooks'
 
-function DeleteButton() {}
+import { Button, Icon } from ''
+
+function DeleteButton() {
+  return (
+    <Button
+      as='div'
+      color='red'
+      floated='right'
+      onClick={() => console.log('Delete post')}
+    >
+      <Icon name='trash' style={{ margin: 0 }} />
+    </Button>
+  )
+}
 
 export default DeleteButton
