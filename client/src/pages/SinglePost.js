58e2f046a441225d5f27cd1ba1ec484433df3eec
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-03 11:35:39 +0200

    imported Button from semantic ui

--- client/src/pages/SinglePost.js
+++ client/src/pages/SinglePost.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import gql from 'graphql-tag'
 import { useQuery } from '@apollo/react-hooks'
-import { Grid, Image, Card } from 'semantic-ui-react'
+import { Grid, Image, Card, Button } from 'semantic-ui-react'
 import moment from 'moment'
 
 import { AuthContext } from '../context/auth'
