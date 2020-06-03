20aa53438b4e82f2c0fab37cfce539cfb9f06675
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-03 12:19:11 +0200

    fixed import of the semantic ui components

--- client/src/components/DeleteButton.js
+++ client/src/components/DeleteButton.js
@@ -2,7 +2,7 @@ import React from 'react'
 import gql from 'graphql-tag'
 import { useMutation } from '@apollo/react-hooks'
 
-import { Button, Icon } from ''
+import { Button, Icon } from 'semantic-ui-react'
 
 function DeleteButton() {
   return (
