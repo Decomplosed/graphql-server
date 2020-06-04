2c1cf45572fe290ebea01c3862c0ee8a87d578ed
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-04 22:33:08 +0200

    imported Form from semantic ui

--- client/src/pages/SinglePost.js
+++ client/src/pages/SinglePost.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react'
 import gql from 'graphql-tag'
 import { useQuery, useMutation } from '@apollo/react-hooks'
-import { Grid, Image, Card, Button, Icon, Label } from 'semantic-ui-react'
+import { Grid, Image, Card, Button, Icon, Label, Form } from 'semantic-ui-react'
 import moment from 'moment'
 
 import { AuthContext } from '../context/auth'
