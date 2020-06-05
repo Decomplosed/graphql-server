1988bdeeb7986989a4f45aeaf36ba523dff95231
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-05 17:15:42 +0200

    imported Popup inside LikeButton

--- client/src/components/LikeButton.js
+++ client/src/components/LikeButton.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { useMutation } from '@apollo/react-hooks'
 import gql from 'graphql-tag'
 
-import { Button, Label, Icon } from 'semantic-ui-react'
+import { Button, Label, Icon, Popup } from 'semantic-ui-react'
 
 function LikeButton({ user, post: { id, likeCount, likes } }) {
   const [liked, setLiked] = useState(false)
